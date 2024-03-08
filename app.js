71a730a969eb21b9f896bd172e602fcacd22109a
Ismail abbasi <ismail.official295@gmail.com>
2024-03-08 06:59:41 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let hrs = document.querySelector("#hour");
+let min = document.querySelector("#min");
+let sec = document.querySelector("#sec");
+
+
+
+setInterval(() => {
+    let currentTime = new Date();
+
+    hrs.innerText = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+    min.innerText = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+    sec.innerText = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+}, 1000)
+
